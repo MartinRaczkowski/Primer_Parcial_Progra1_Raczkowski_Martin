218a59c48766b0591139b65867c7c72c80499e33
MartinRaczkowski <raczkowskimartin49@gmail.com>
2024-10-14 22:06:04 -0300

    Progreso hasta ahora

--- funciones/funciones.py
+++ funciones/funciones.py
@@ -1,4 +1,3 @@
-from funciones.auxiliares import encontrar_minimo
 #1 - Obtener existencias: para ello deberá crear una función que cargue la existencia de cada vehículo en todos los garajes y mostrarlos.
 
 def obtener_existencias(matriz: list[list]) -> list[list]:
@@ -31,6 +30,80 @@ def datos_garaje_con_menor_unidades(matriz: list[list]):
 
     print(f'El garaje que almacena menos unidades es el {garaje + 1} con {minimo} unidades')
 
+#4 - Máxima cantidad de unidades almacenadas entre todos los garajes.
+
+def maxima_cantidad_almacenada(matriz: list[list]):
+
+    for i in range(len(matriz)):
+        if i == 0:
+            maximo = matriz[i][2]
+        elif maximo < matriz[i][2]:
+            maximo = matriz[i][2]
+    
+    print(f'La cantidad maxima almacenada en un solo garaje es de {maximo} vehiculos')
+
+#5 - Obtener recaudación: para ello deberás crear una función que calcule la recaudación de cada garaje, 
+# teniendo en cuenta su precio unitario y cantidad de unidades almacenadas en cada garaje.
+
+def obtener_recaudacion(matriz: list[list]):
+
+    for i in range(len(matriz)):
+        matriz[i][4] = matriz[i][2] * matriz[i][3]
+        print(f'La recaudacion del garaje {i + 1} es de {matriz[i][4]}')
+
+#6 - Cantidad de garajes que hayan almacenado 6 o más unidades de vehículos.
+
+def cantidad_garajes_mayor_unidades(matriz: list[list]):
+
+    total_garajes = 0
+    for i in range(len(matriz)):
+        if matriz[i][2] >= 6:
+            total_garajes += 1
+
+    print(f'La cantidad de garajes con 6 o mas unidades es {total_garajes}')
+
+#7 - Porcentaje de unidades de cada marca de vehículo sobre el total de vehículos almacenados entre todos los garajes de la sede matriz. 
+# Además mostrar todos los datos del garaje con el máximo porcentaje de vehículos almacenados.
+
+def porcentajes_de_unidades_por_marca(matriz: list[list]):
+
+    marcas = []
+    for i in range(len(matriz)):
+        if matriz[i][0] not in marcas:
+            marcas.append(matriz[i][0])
+    
+    total_unidades_por_marca = [0] * len(marcas)
+    total_unidades_en_concesionaria = 0
+
+    for i in range(len(matriz)):
+        for j in range(len(marcas)):
+            if matriz[i][0] == marcas[j]:
+                total_unidades_por_marca[j] += matriz[i][2]
+        total_unidades_en_concesionaria += matriz[i][2]
+
+    for i in range(len(matriz)):
+        for j in range(len(marcas)):
+            if matriz[i][0] == marcas[j]:
+                maximo_porcentaje_garaje_por_marca = matriz[i][2] / total_unidades_por_marca[j] * 100
+                garaje_maximo_porcentaje = i
+            elif maximo_porcentaje_garaje_por_marca < (matriz[i][2] / total_unidades_por_marca[j] * 100):
+                maximo_porcentaje_garaje_por_marca = matriz[i][2] / total_unidades_por_marca[j] * 100
+                garaje_maximo_porcentaje = i
+
+    for i in range(len(marcas)):
+        print(f'La marca {marcas[i]} tiene un porcentaje de {total_unidades_por_marca[i] / total_unidades_en_concesionaria * 100} sobre el total de vehiculos almacenados en la concesionaria')
+
+    print(f"""El garaje con mayor porcentaje de su marca es el {garaje_maximo_porcentaje + 1}
+Marca: {matriz[garaje_maximo_porcentaje][0]}
+Modelo: {matriz[garaje_maximo_porcentaje][1]}
+Unidades: {matriz[garaje_maximo_porcentaje][2]}
+Precio: {matriz[garaje_maximo_porcentaje][3]}
+Ganancia: {matriz[garaje_maximo_porcentaje][4]}
+""")
+    
+#8 - Generar un informe con la recaudación de cada garaje, ordenada de mayor a menor.
+
+#def generar_informe_ganacias(matriz: list[list]):
 
 
 
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from UTN_Heroes_Dataset.utn_pp import get_original_matrix, mostrar_matriz_texto_tabla, clear_console, color_text
-from funciones.funciones import obtener_existencias
+from funciones.funciones import obtener_existencias, porcentajes_de_unidades_por_marca
 from funciones.auxiliares import cambiar_filas_por_columnas
 
 matriz_concesionaria = cambiar_filas_por_columnas(get_original_matrix())
@@ -8,7 +8,7 @@ nombres_columnas = ['Marca', 'Modelo', 'Unidades', 'Precio', 'Ganancia']
 if __name__ == '__main__':
     
     mostrar_matriz_texto_tabla(matriz_concesionaria, nombres_columnas)
-    obtener_existencias(matriz_concesionaria)
+    #obtener_existencias(matriz_concesionaria)
 
-    
+    porcentajes_de_unidades_por_marca(matriz_concesionaria)
 
